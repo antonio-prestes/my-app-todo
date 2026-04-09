903da9bdc93fa5f37b4804cc0c4ff37c4efa1d8b
antonio-prestes <diegoantoniodasp@hotmail.com>
2026-04-09 15:40:56 -0300

    feat: initial commit

---
+++ src/app/components/layout/SideBar.tsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <h1 className="text-3xl font-bold text-red-500">SideBar</h1>
+    </div>
+  );
+}
